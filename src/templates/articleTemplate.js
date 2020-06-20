4db76b4329a91ce57dfe968090245ef17cda0ab7
willjw3 <will.j.ward3@gmail.com>
2020-06-20 11:59:57 +0900

    Testing local storage netlify

--- src/templates/articleTemplate.js
+++ src/templates/articleTemplate.js
@@ -7,32 +7,32 @@ import "../styles/article.scss";
 const Article = ({data}) => {
     const post = data.markdownRemark;
     console.log(post)
-    const [darkMode, setDarkMode] = useState(typeof window !== 'undefined' ? getInitialMode() : false);
+    const [darkMode, setDarkMode] = useState(true);
     useEffect(() => {
         localStorage.setItem("dark", JSON.stringify(darkMode))
     }, [darkMode])
 
-    function getInitialMode() {
-        const isReturningUser = "dark" in localStorage;
-        const savedMode = JSON.parse(localStorage.getItem("dark"));
-        const userPrefersDark = getPrefColorScheme();
-        // if mode was saved -> dark / light
-        if (isReturningUser) {
-            return savedMode;
-            // if preferred color scheme is dark -> dark
-        } else if (userPrefersDark) {
-            return true;
-            // otherwise -> light
-        } else {
-            return false;
-        }  
-    }
+    // function getInitialMode() {
+    //     const isReturningUser = "dark" in localStorage;
+    //     const savedMode = JSON.parse(localStorage.getItem("dark"));
+    //     const userPrefersDark = getPrefColorScheme();
+    //     // if mode was saved -> dark / light
+    //     if (isReturningUser) {
+    //         return savedMode;
+    //         // if preferred color scheme is dark -> dark
+    //     } else if (userPrefersDark) {
+    //         return true;
+    //         // otherwise -> light
+    //     } else {
+    //         return false;
+    //     }  
+    // }
 
-    function getPrefColorScheme() {
-        if (!window.matchMedia) return;
+    // function getPrefColorScheme() {
+    //     if (!window.matchMedia) return;
 
-        return window.matchMedia("(prefers-color-scheme: dark)").matches;
-    }
+    //     return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    // }
 
     return (
         <Layout>
